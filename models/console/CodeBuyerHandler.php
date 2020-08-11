44c93f4d6c89b5aa909168495db7246c1dc15b92
diver90 <mihail.zavalniuk@gmail.com>
2020-08-11 22:13:10 +0300

    fix buy bot send pay message 4

--- models/console/CodeBuyerHandler.php
+++ models/console/CodeBuyerHandler.php
@@ -70,20 +70,14 @@ class CodeBuyerHandler extends EventHandler
 
             $this->update = $update;
 //buttons
-            print_r('1');
-
             if (isset($update['message']['reply_markup']['rows'])) {
                 foreach ($update['message']['reply_markup']['rows'] as $row) {
                     foreach ($row['buttons'] as $button) {
                         if ($button['text'] === '🤝 I have paid') {
                             $this->buttonPaid = $button;
-                            print_r('2');
                         }
-                        print_r('3');
                     }
-                    print_r('4');
                 }
-                print_r('5');
             }
 
             $message = $update['message']['message'];
@@ -97,27 +91,22 @@ class CodeBuyerHandler extends EventHandler
             if (preg_match("/^Stop$/i", $message)) {
                 $this->stop();
             }
-            print_r('6');
             if (preg_match("/^Start$/i", $message)) {
                 $this->status = self::NORMAL_CYCLE;
                 yield $this->sendMessage('🔎 Orderbook UAH', 5);
             }
-            print_r('7');
             if (preg_match("/^Paid$/i", $message)) {
                 yield $this->buttonPaid->click();
             }
-            print_r('8');
             if (preg_match("/^Cancel$/i", $message)) {
                 $this->status = self::NORMAL_CYCLE;
                 $this->activeDeal = [];
                 yield $this->sendMessage('❌ Cancel deal', 0);
             }
-            print_r('9');
             if (preg_match("/\bPress “1” to cancel the deal\b/i", $message)) {
                 $this->activeDeal = [];
                 yield $this->sendMessage('1', 0);
             }
-            print_r('10');
 
             if (preg_match("/\bAnother deal has already been made based on this order\b/i", $message)) {
                 $this->activeDeal = [];
@@ -127,11 +116,9 @@ class CodeBuyerHandler extends EventHandler
                 $this->status = self::NORMAL_CYCLE;
                 yield $this->sendMessage('🔎 Orderbook UAH', 5);
             }
-            print_r('11');
             if (preg_match("/\bCommand not recognized\b/i", $message)) {
                 return;
             }
-            print_r('12');
             if (preg_match("/\bOrder is not found\b/i", $message)) {
                 $this->activeDeal = [];
                 $this->model->setActiveDealStatus('failure');
@@ -139,24 +126,20 @@ class CodeBuyerHandler extends EventHandler
                 $this->status = self::NORMAL_CYCLE;
                 yield $this->sendMessage('🔎 Orderbook UAH', 5);
             }
-            print_r('13');
             if (preg_match("/\bin a deal creation\b/i", $message)) {
                 $this->warnAdmin = true;
                 $this->status = self::NORMAL_CYCLE;
                 yield $this->sendMessage('Создание сделки ' . $this->activeDeal['order_id']);
             }
-            print_r('14');
             if (preg_match("/\bPlease pay\b/i", $message)) {
                 $this->warnAdmin = true;
                 $this->status = self::NORMAL_CYCLE;
                 yield $this->sendMessage('Сделка создана успешно, сумма к оплате ' . $this->activeDeal['price']);
             }
-            print_r('15');
             if (preg_match("/^\d{16}$/i", $message)) {
                 $this->warnAdmin = true;
                 yield $this->sendMessage($message);
             }
-            print_r('16');
             if (preg_match("/^[\d\w-]{53}-UAH-KCode$/", $message)) {
                 $this->warnAdmin = true;
                 $this->model->setActiveDealStatus('successful');
@@ -164,12 +147,10 @@ class CodeBuyerHandler extends EventHandler
                 $this->activeDeal = [];
                 yield $this->sendMessage($message);
             }
-            print_r('17');
             if (preg_match("/\bThe validity period for Deal\b/i", $message)) {
                 $this->warnAdmin = true;
                 yield $this->sendMessage('Сделка в ожидании!');
             }
-            print_r('18');
             /*if (preg_match("/\byou are going to accept order\b/i", $message) && $this->status !== self::PAY_SENDED) {
                 $this->status = self::PAY_SENDED;
                 //dump('Pay send');
@@ -179,24 +160,20 @@ class CodeBuyerHandler extends EventHandler
             if (preg_match("/\bBuy this code\b/im", $message)) {
                 $this->model->setMessage($message);
                 $answer = $this->model->countOrders();
-                print_r('18');
                 if (!$answer) {
                     $this->activeDeal = [];
                     yield $this->sendMessage('🔎 Orderbook UAH', 5);
-                    print_r('19');
                 } elseif (empty($this->activeDeal) || $this->activeDeal['order_id'] !== $answer['order_id']) {
                     $this->activeDeal = $answer;
                     $this->status = self::DEAL_SENDED;
                     yield $this->sendPayMessage($answer['order_id']);
                     //yield $this->sendMessage('/deal' . $answer['order_id']);
-                    print_r('20');
                 } else {
                     var_dump($answer);
                     var_dump($this->status);
                 }
             }
         }
-        print_r('21');
         return;
 
     }
@@ -230,7 +207,7 @@ class CodeBuyerHandler extends EventHandler
         $peer = $this->warnAdmin ? self::ADMIN : self::BOT;
         try {
             $this->messages->sendMessage(['peer' => '@' . $peer, 'message' => '/deal' . $order_id, 'parse_mode' => 'HTML']);
-            sleep(0.2);
+            sleep(0.3);
             yield $this->messages->sendMessage(['peer' => '@' . $peer, 'message' =>  '📥 Pay', 'parse_mode' => 'HTML']);
            // yield $this->messages->sendMessage(['multiple' => true, ['peer' => '@' . $peer, 'message' => $messages[0], 'parse_mode' => 'HTML'], ['peer' => '@' . $peer, 'message' => $messages[1], 'parse_mode' => 'HTML']]);
         } catch (RPCErrorException $e) {
